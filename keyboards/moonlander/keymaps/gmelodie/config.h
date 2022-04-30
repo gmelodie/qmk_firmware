03a0b45a25711233e03d9826fa9e955c31c633f0
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2022-04-30 11:00:43 -0300

    add PERMISSIVE_HOLD and IGNORE_MOD_TAP_INTERRUPT

--- keyboards/moonlander/keymaps/gmelodie/config.h
+++ keyboards/moonlander/keymaps/gmelodie/config.h
@@ -3,7 +3,14 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+#undef DEBOUNCE
+#define DEBOUNCE 5
+
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"XJNWy/Kg3za"
+#define FIRMWARE_VERSION u8"XJNWy/w4qZ3"
 #define RGB_MATRIX_STARTUP_SPD 60
-#define TAPPING_TERM 135
+#define TAPPING_TERM 125
+/* #define TAPPING_TERM_PER_KEY */
+/* #define PERMISSIVE_HOLD_PER_KEY */
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
