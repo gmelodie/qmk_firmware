983ad16b9ca9ccb4cbd132c6c70dc4a73f61599a
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2023-10-05 14:14:55 -0300

    I am even stupider than I thought

--- keyboards/moonlander/keymaps/gmelodie/config.h
+++ keyboards/moonlander/keymaps/gmelodie/config.h
@@ -9,7 +9,7 @@
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define FIRMWARE_VERSION u8"XJNWy/w4qZ3"
 #define RGB_MATRIX_STARTUP_SPD 60
-#define TAPPING_TERM 120
+#define TAPPING_TERM 125
 
 /* Things for "home row" modifiers */
 #define IGNORE_MOD_TAP_INTERRUPT
